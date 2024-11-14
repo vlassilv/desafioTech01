ee928b0affcb6d264cc40379ccbaa4c4fa0d0d7e
vlassilv <valmirsystem@gmail.com>
2024-11-14 10:08:15 -0300

    pageObject update

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,22 @@
-const cucumber = require('cypress-cucumber-preprocessor').default;
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
+const path = require("path");
 
-module.exports = {
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://demoqa.com',
-    specPattern: 'cypress/e2e/features/*.feature',
-    setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber());
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on('file:preprocessor', bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
     },
-    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/features/**/*.feature',
+    supportFile: 'cypress/support/e2e.js', // Arquivo de suporte com as importações
+    stepDefinitions: path.resolve(__dirname, 'cypress/e2e/step_definitions/**/*.js'),
   },
-};
+});
